8ce87cb9d56f7f07e53e597e16b3b2779708bd2a
Klesk <klesk@whatever.com>
2019-12-18 23:55:51 +0200

    updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,10 +4,6 @@ import slide_1 from '../assets/slide_1.jpg';
 import slide_2 from '../assets/slide_2.jpg';
 import slide_3 from '../assets/slide_3.jpg';
 import slide_4 from '../assets/slide_4.jpg';
-import book from '../assets/open-book.png';
-import video from '../assets/video.png';
-import music from '../assets/musical-note.png';
-
 
 function Home() {
     const [slideIndex, setSlideIndex] = useState(1);
@@ -50,7 +46,6 @@ function Home() {
         }
     }, [slideIndex]);
     
-
     useEffect(() => {
         function handleResize() {
             setHeight(getSize());
@@ -61,16 +56,14 @@ function Home() {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-
     return (
         <Fragment>
             <div className="container">
-            
                 <div className="slideshow-container" onClick={(event) => handleSlideClick(event)}>
                     <div className="slides-container" style={{height: height + "px"}}>
                         {images.map((item) => (
                             <div key={item.id} className={slideIndex === item.id ? "slide active" : "slide"}>
-                                <img src={item.img} alt="smartphones" />
+                                <img src={item.img} className="slide__img" alt="smartphones" />
                             </div>
                         ))}
                         <span className="slide-prev">&#10094;</span>
@@ -104,24 +97,24 @@ function Home() {
                     </div>
                 </section>
 
-                <section className="services">
+                <section className="service">
                     <div className="flex-row">
                         <div className="flex-col-4">
-                            <div className="service-wrapper">
-                        
-                                <h3>Dedicated Development Teams</h3>
+                            <div className="service__wrapper">
+                                <div className="service__icon service__icon--book">Book</div>
+                                <h3 className="service__title">Books</h3>
                             </div>
                         </div>
                         <div className="flex-col-4">
-                            <div className="service-wrapper">
-                                <i className="fas fa-mobile-alt"></i>
-                                <h3>Mobile App Development</h3>
+                            <div className="service__wrapper">
+                                <div className="service__icon service__icon--video">Video</div>
+                                <h3 className="service__title">Video</h3>
                             </div>
                         </div>
                         <div className="flex-col-4">
-                            <div className="service-wrapper">
-                                <i className="fas fa-laptop"></i>
-                                <h3>Enterprise Software Development</h3>
+                            <div className="service__wrapper">
+                                <div className="service__icon service__icon--music">Music</div>
+                                <h3 className="service__title">Music</h3>
                             </div>
                         </div>
                     </div>
@@ -130,38 +123,58 @@ function Home() {
                 <section className="map-box">
                     <div className="flex-row">
                         <div className="flex-col-8">
-                            <div id="map" style={{width: '100%', minHeight: '450px'}}></div>
+                            <div className="map" style={{width: '100%', minHeight: '450px'}}></div>
                         </div>
                         <div className="flex-col-4">
                             <div className="company-contacts">
-                                <p className="company-contacts-city">San Francisco</p>
-                                <p>
+                                <p className="company-contacts__city">San Francisco</p>
+                                <p className="company-contacts__text">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing esi elit. Vivamus at arcu sem.
                                     Vestibulum ornare eleifendit massa, nec tempor odio. Fusce posuere nunc iaculis ligula
                                     viverra iaculis. Aliquam erat volutpat.
                                 </p>
-                                <ul className="company-address-wrapper">
+                                <ul className="company-contacts__address">
                                     <li>
-                                        <i className="fas fa-map-marker-alt"></i> Millennium Tower, 301 Mission St, San Francisco, CA 94105, US
+                                        Millennium Tower, 301 Mission St, San Francisco, CA 94105, US
                                     </li>
                                     <li>
-                                        <a href="tel:0123-456-789" rel="nofollow"><i className="fas fa-phone-alt"></i> 0123-456-789</a>
+                                        <a href="tel:0123-456-789" rel="nofollow">0123-456-789</a>
                                     </li>
                                     <li>
-                                        <a href="#mysite"><i className="fas fa-link"></i> www.yourwebsite.com</a>
+                                        <a href="#mysite">www.yourwebsite.com</a>
                                     </li>
                                     <li>
-                                        <a href="mailto:support@yourwebsite.com"><i className="far fa-envelope"></i> support@yourwebsite.com</a>
+                                        <a href="mailto:support@yourwebsite.com">support@yourwebsite.com</a>
                                     </li>
                                     <li>
-                                        <i className="far fa-clock"></i> Monday-Friday: 9am to 6pm Saturday: 10am to 6pm Sunday: 10am to 2pm
+                                        Monday-Friday: 9am to 6pm Saturday: 10am to 6pm Sunday: 10am to 2pm
                                     </li>
                                 </ul>
                                 <ul className="social-links">
-                                    <li><a href="#twitter" target="_blank"><i className="fab fa-twitter"></i></a></li>
-                                    <li><a href="#facebook" target="_blank"><i className="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#google-plus" target="_blank"><i className="fab fa-google-plus-g"></i></a></li>
-                                    <li><a href="#instagram" target="_blank"><i className="fab fa-instagram"></i></a></li>
+                                    <li>
+                                        <a href="#twitter" 
+                                           className="social-link social-link--twitter" 
+                                           target="_blank" 
+                                           title="Twitter">Twitter</a>
+                                    </li>
+                                    <li>
+                                        <a href="#facebook" 
+                                           className="social-link social-link--facebook" 
+                                           target="_blank" 
+                                           title="Facebook">Facebook</a>
+                                    </li>
+                                    <li>
+                                        <a href="#google-plus" 
+                                           className="social-link social-link--instagram" 
+                                           target="_blank" 
+                                           title="Instagram">Instagram</a>
+                                    </li>
+                                    <li>
+                                        <a href="#instagram" 
+                                           className="social-link social-link--google" 
+                                           target="_blank" 
+                                           title="Google plus">Google plus</a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
